0ab8c575408709b4df2589e5841c2860453842a8
Jacksuyl <syl19930927@gmail.com>
2024-06-06 20:35:21 -0700

    fixed seeder-demo

--- backend/db/seeders/20240605013925-demo-user.js
+++ backend/db/seeders/20240605013925-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName:'Demo',
+        lastName:'users',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName:'Demo',
+        lastName:'users',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName:'Demo',
+        lastName:'users',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
